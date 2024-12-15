e292bb1d1197951b8c097038ee1beac2b1ff1ae6
Anshuman Tiwari <113034088+anshumanat@users.noreply.github.com>
2024-12-15 16:09:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if not file_path.exists():
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
-st.write("Loaded credentials:", credentials)
+st.write(hashed_passwords)
 
 
 # Prepare credentials dictionary with email field included
